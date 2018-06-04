bd68f21267c21bb98fdea4064316559bceb4c692
Szymon <mare8e@gmail.com>
2018-06-04 20:56:51 +0200

    added socket creation

--- PythonServer.py
+++ PythonServer.py
@@ -1,2 +1,15 @@
 from socket import *
 from threading import Thread
+
+host = "127.0.0.1"
+port = 8000
+
+s = socket(AF_INET,SOCK_STREAM) #create socket
+s.bind((host,port))
+s.listen(1) #number of connections available
+
+connection_id, ip_address = s._accept()
+
+print ('Connection with client:', ip_address)
+
+
